428eab4ec76ee9c380a194ca8b4da3f3dfea16fc
unknown <guptasuchismita96@gmail.com>
2022-11-08 16:16:37 +0530

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
 
 app.use(express.static(publicDirPath))
 
@@ -10,6 +11,6 @@ app.get('', (res, req) =>{
     res.send('<h1>Weather</h1>')
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log('listing to port: 3000');
 })
